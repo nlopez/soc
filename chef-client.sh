65e5bd556800a14ef545da8080d513e89d12c4a4
Nick Lopez <nlopez@gmail.com>
2013-04-25 18:55:15 -0400

    use the latest chef solo installer

--- chef-client.sh
+++ chef-client.sh
@@ -2,3 +2,4 @@
 mkdir -p /var/lib/ohai/plugins && cp /vagrant/vagrant_eth1.rb /var/lib/ohai/plugins/vagrant_eth1.rb
 mkdir -p /etc/chef && cp "/vagrant/.cache/validation.pem" "/etc/chef/validation.pem"
 /usr/sbin/useradd chef --system -d /tmp -M -s /bin/false || /bin/true
+bash /vagrant/chef-solo-install.sh
