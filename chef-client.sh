e1aff19ef7d5c051d13928e5b7a7a93ac5be63f0
Nick Lopez <nlopez@gmail.com>
2013-04-25 19:07:12 -0400

    fixes hosts order

--- chef-client.sh
+++ chef-client.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo 'chef-server 192.168.168.168' >> /etc/hosts
+echo '192.168.168.168 chef-server' >> /etc/hosts
 mkdir -p /var/lib/ohai/plugins && cp /vagrant/vagrant_eth1.rb /var/lib/ohai/plugins/vagrant_eth1.rb
 mkdir -p /etc/chef && cp "/vagrant/.cache/validation.pem" "/etc/chef/validation.pem"
 /usr/sbin/useradd chef --system -d /tmp -M -s /bin/false || /bin/true
