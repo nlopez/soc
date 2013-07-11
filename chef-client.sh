0f0830ac479f317778321c3c0b4bcd430492ed14
Anurag Mohanty <amohanty@amplify.com>
2013-07-11 17:05:17 -0400

    readded chef-solo.sh to the mix

--- chef-client.sh
+++ chef-client.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+echo '192.168.168.168 chef-server' >> /etc/hosts	
 mkdir -p /var/lib/ohai/plugins && cp /vagrant/vagrant_eth1.rb /var/lib/ohai/plugins/vagrant_eth1.rb
 mkdir -p /etc/chef && cp "/vagrant/.cache/validation.pem" "/etc/chef/validation.pem"
 cp /vagrant/.cache/encrypted_data_bag_secret /etc/chef/encrypted_data_bag_secret 
 /usr/sbin/useradd chef --system -d /tmp -M -s /bin/false || /bin/true
+bash /vagrant/chef-solo-install.sh
